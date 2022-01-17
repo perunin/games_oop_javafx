d10b0a9c55603ed12900492c9db37d2013678ea2
perunin <perunin@icloud.com>
2022-01-17 10:49:36 +0300

    2. Сокобан. Тестовое задание v2

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -22,7 +22,7 @@ public class Win {
         return result;
     }
 
-    public static boolean monoVertical (int[][] board, int column) {
+    public static boolean monoVertical(int[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
             if (board[i][column] != 1) {
